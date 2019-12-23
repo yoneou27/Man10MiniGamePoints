4868b04cae7dd663c174f5ba3cdfa455f3f5cb72
BuildTools <unconfigured@null.spigotmc.org>
2019-12-23 19:39:34 +0900

    if not existsのミスを修正

--- src/main/java/yoneyone/yone/yo/man10minigamepoints/MySQLManager.java
+++ src/main/java/yoneyone/yone/yo/man10minigamepoints/MySQLManager.java
@@ -66,7 +66,7 @@ public class MySQLManager {
                 "ENGINE=InnoDB\n" +
                 "AUTO_INCREMENT=1\n" +
                 ";\n");
-        execute("CREATE TABLE `exchange_items` (\n" +
+        execute("CREATE TABLE　if not exists `exchange_items` (\n" +
                 "\t`id` INT(11) NOT NULL AUTO_INCREMENT,\n" +
                 "\t`item` TEXT NOT NULL,\n" +
                 "\t`name` VARCHAR(50) NOT NULL,\n" +
